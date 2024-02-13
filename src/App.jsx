5178a6a4ba378e824c67f933b598f002af8824bb
shwetank-tripathi <swetankvns@gmail.com>
2024-02-14 00:56:49 +0530

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './Screens/HomePage'
+import AboutPage from './Screens/AboutPage'
+import Layout from './Layout'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <img src="" alt='' ></img>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/About" element={<AboutPage />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
