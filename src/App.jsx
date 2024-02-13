4300404ecb501c2fed71046f01e23fff0a755f82
shwetank-tripathi <swetankvns@gmail.com>
2024-02-14 00:51:36 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './Screens/HomePage'
-import AboutPage from './Screens/AboutPage'
-import Layout from './Layout'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/About" element={<AboutPage />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        <img src="" alt='' ></img>
+      </div>
+    </>
   )
 }
 
