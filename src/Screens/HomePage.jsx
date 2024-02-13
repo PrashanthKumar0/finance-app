fb0efe1dd63e79687bc1aaf2f6a3e3e2aeef5cbf
shwetank-tripathi <swetankvns@gmail.com>
2024-02-14 02:00:04 +0530

    latest

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className="bg-black h-[100vh] text-white" style={{
+      background:"radial-gradient(circle at 100% 200%,midnightblue,black)"
+    }}>
+      <div className="flex flex-col gap-4 ">
+        <div className='font-bold text-5xl'>slogan</div>
+        <div><p>Empowering Your Financial Journey</p></div>
+      </div>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav className="bg-white sticky border-gray-200 dark:bg-gray-900">
+    <nav className=" sticky top-0 bg-black shadow-xl shadow-black/50 backdrop-blur-lg">
       <div className="max-w-screen-xl flex flex-wrap gap-4 items-center justify-between  p-4">
         <a
           href="https://flowbite.com/"
@@ -22,11 +22,11 @@ function NavBar() {
           className="ml-6 mr-auto items-center justify-between hidden w-full md:flex md:w-auto md:order-1 text-xs"
           id="navbar-user"
         >
-          <ul className="flex flex-col font-medium md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col md:p-0 mt-4 md:space-x-4 md:flex-row md:mt-0 md:border-0 bg-black dark:border-gray-700">
             <li>
               <a
                 href="#"
-                className=" block py-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                className="block py-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
