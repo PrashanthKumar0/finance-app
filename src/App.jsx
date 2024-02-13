e432a3b0812209ad1f4d68cceb9932e54016838d
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-02-13 23:45:46 +0530

    basic routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './Screens/HomePage'
+import AboutPage from './Screens/AboutPage'
+import Layout from './Layout'
 
 function App() {
 
   return (
-    <>
-      Hello World.
-      gj
-      <div>
-        <img src="" alt='' ></img>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route location="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route location="/About" element={<AboutPage />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
