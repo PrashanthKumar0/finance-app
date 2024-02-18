042dd145b46cd264cb4635f7c71b02bc52f641d0
SanskarThakur5251 <160384232+SanskarThakur5251@users.noreply.github.com>
2024-02-18 16:37:51 +0530

    Tiny Tweak in scroll bar

--- src/components/ScrollIndecator.jsx
+++ src/components/ScrollIndecator.jsx
@@ -11,7 +11,7 @@ function ScrollIndecator() {
     }, [])
 
     return (
-        <div className='fixed z-10 right-0 top-1/2 h-[300px] w-[12px] md:w-[20px] translate-y-[-50%]'>
+        <div className='fixed z-10 right-0 top-1/2 h-[300px] w-[12px] md:w-[20px] translate-y-[-50%] opacity-70'>
             <div class="h-full bg-gray-200 rounded-full w-0.5 mb-4 dark:bg-gray-700">
                 <div ref={scrollRef} class="bg-gray-600 w-0.5 rounded-full dark:bg-gray-300" ></div>
             </div>
