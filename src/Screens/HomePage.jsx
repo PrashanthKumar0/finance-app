ad246a79ecd17419b05fc532762f8ecc50552728
SanskarThakur5251 <160384232+SanskarThakur5251@users.noreply.github.com>
2024-02-19 00:29:09 +0530

    responsive home screen

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -112,11 +112,11 @@ function HomePage() {
             <div className="text-center text-3xl my-5 py-auto">
               Frequently Asked Questions
             </div>
-            <FAQ></FAQ>
+            <FAQ/>
           </div>
         </div>
 
-        <Divider className="w-[800px] mx-auto" />
+        {/* <Divider className="w-[800px] mx-auto" /> */}
         {/* companies-trust */}
 
 
@@ -156,7 +156,7 @@ function HomePage() {
             </div>
           </div>
         </div> */}
-        <Divider className="w-[800px] mx-auto" />
+        {/* <Divider className="w-[800px] mx-auto" /> */}
         {/* Reach Us */}
         <ReachUs />
         {/* Footer */}
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
 export default function FAQ() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
   return (
     <Accordion variant="splitted">
       <AccordionItem
