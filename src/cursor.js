83225e3a284fce39bc8df3e5a0d452cfafe78ccb
SanskarThakur5251 <160384232+SanskarThakur5251@users.noreply.github.com>
2024-02-19 01:43:58 +0530

    hide custom cursor on mobile

--- src/cursor.js
+++ src/cursor.js
@@ -1,5 +1,8 @@
 addEventListener('mousemove', (e) => {
     const cursor = document.getElementById('cursor');
+    if (/Android|webOS|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent)) {
+        cursor.style.opacity = '0';
+    }
     if (!cursor) {
         console.log("no cursor")
         return;
